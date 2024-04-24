8781564d8ab117cafbb98b595cf8135de8e7468c
Davin Putra F <123059312+VinLvy@users.noreply.github.com>
2024-04-24 18:45:39 +0700

    Add files via upload

---
+++ function_parameter.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func sayhelloto(firstname string, lastname string) {
+	fmt.Println("hello", firstname, lastname)
+}
+
+func para() {
+	sayhelloto("daoa", "rendra")
+	sayhelloto("daffa", "rendri")
+}
