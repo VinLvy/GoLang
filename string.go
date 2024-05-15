441e91b9db319255a0923e33feeaf4970804e08a
Davin Putra F <123059312+VinLvy@users.noreply.github.com>
2024-05-15 11:29:23 +0700

    Add files via upload

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func strng() {
+	fmt.Println("Daoa")
+	fmt.Println("Daoa Rendri")
+	fmt.Println("Daoa Rendri Putri")
+
+	fmt.Println(len("Daoa"))
+	fmt.Println("Daoa Rendri"[0])
+	fmt.Println("Daoa Rendri Putri"[1])
+}
